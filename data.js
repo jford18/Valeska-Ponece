d469564c4028c003f285de098ed11599aab12c47
jford18 <67157718+jford18@users.noreply.github.com>
2025-09-26 17:43:04 -0500

    Refactor landing page for Valeska Ponce

---
+++ data.js
@@ -0,0 +1,96 @@
+const productos = [
+  {
+    id: 1,
+    nombre: 'Anillo Aurora',
+    categoria: 'Anillos',
+    material: 'Oro',
+    precio: 405.0,
+    nuevo: true,
+    imagenes: [
+      'https://images.pexels.com/photos/267202/pexels-photo-267202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    ]
+  },
+  {
+    id: 2,
+    nombre: 'Anillo Diamante',
+    categoria: 'Anillos',
+    material: 'Plata',
+    precio: 490.0,
+    nuevo: false,
+    imagenes: ['https://images.pexels.com/photos/265906/pexels-photo-265906.jpeg']
+  },
+  {
+    id: 3,
+    nombre: 'Collar Dorado',
+    categoria: 'Collares',
+    material: 'Oro',
+    precio: 320.0,
+    nuevo: true,
+    imagenes: ['https://images.pexels.com/photos/1457801/pexels-photo-1457801.jpeg']
+  },
+  {
+    id: 4,
+    nombre: 'Aretes Colgantes',
+    categoria: 'Aretes',
+    material: 'Oro',
+    precio: 180.0,
+    nuevo: false,
+    imagenes: ['https://images.pexels.com/photos/1191531/pexels-photo-1191531.jpeg']
+  },
+  {
+    id: 5,
+    nombre: 'Pulsera Minimal',
+    categoria: 'Pulseras',
+    material: 'Plata',
+    precio: 210.0,
+    nuevo: true,
+    imagenes: ['https://images.pexels.com/photos/3641059/pexels-photo-3641059.jpeg']
+  },
+  {
+    id: 6,
+    nombre: 'Set Joyas Blancas',
+    categoria: 'Collares',
+    material: 'Oro',
+    precio: 540.0,
+    nuevo: false,
+    imagenes: ['https://images.pexels.com/photos/1457807/pexels-photo-1457807.jpeg']
+  },
+  {
+    id: 7,
+    nombre: 'Collar Piedras',
+    categoria: 'Collares',
+    material: 'Plata',
+    precio: 399.0,
+    nuevo: true,
+    imagenes: ['https://images.pexels.com/photos/461956/pexels-photo-461956.jpeg']
+  },
+  {
+    id: 8,
+    nombre: 'Set Completo',
+    categoria: 'Collares',
+    material: 'Oro',
+    precio: 600.0,
+    nuevo: false,
+    imagenes: ['https://images.pexels.com/photos/2698456/pexels-photo-2698456.jpeg']
+  },
+  {
+    id: 9,
+    nombre: 'Reloj Lujoso',
+    categoria: 'Accesorios',
+    material: 'Acero',
+    precio: 720.0,
+    nuevo: false,
+    imagenes: ['https://images.pexels.com/photos/3641058/pexels-photo-3641058.jpeg']
+  },
+  {
+    id: 10,
+    nombre: 'Anillos Mesa',
+    categoria: 'Anillos',
+    material: 'Oro',
+    precio: 310.0,
+    nuevo: true,
+    imagenes: ['https://images.pexels.com/photos/842546/pexels-photo-842546.jpeg']
+  }
+];
+
+export { productos };
